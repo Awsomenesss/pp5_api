3ee3a82cfb1b3dc3f5eaae69d7fa743fbd0c7876
Awsomenesss <>
2023-08-22 16:24:38 +0000

    added extra data by annotating querysets

--- event/serializers.py
+++ event/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from event.models import Event
+from likes.models import Like
 
 
 class EventSerializer(serializers.ModelSerializer):
@@ -7,15 +8,25 @@ class EventSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, event=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         model = Event
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at', 'image', 'description',
-            'date', 'time', 'location'
+            'date', 'time', 'location', 'like_id',
         ]
