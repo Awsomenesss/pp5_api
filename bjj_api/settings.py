01141a75c7b8ba5af83943d1744f36d1cf916bd5
Awsomenesss <>
2023-08-24 11:22:44 +0000

    deployment db config fixed

--- bjj_api/settings.py
+++ bjj_api/settings.py
@@ -146,14 +146,15 @@ WSGI_APPLICATION = 'bjj_api.wsgi.application'
 
 if 'DEV' in os.environ:
     DATABASES = {
-        'default': ({
+        'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
-        } if 'DEV' in os.environ else dj_database_url.parse(
-            os.environ.get('DATABASE_URL')
-        ))
+        }
     }
-
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
