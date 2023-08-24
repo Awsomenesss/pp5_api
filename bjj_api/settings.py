dce70d19c29effa57636968bc524be7ffbd9a9b1
Awsomenesss <>
2023-08-24 11:34:23 +0000

    fixed views.py/bjj_api unicode zero width space

--- bjj_api/settings.py
+++ bjj_api/settings.py
@@ -62,9 +62,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [
-    'https://pp5-bjj-api-2269f4220822.herokuapp.com /',
-    '8000-awsomenesss-pp5-api-xilsbox1b5.us2.codeanyapp.com',
+ALLOWED_HOSTS = [os.environ.get(
+    'ALLOWED_HOST'), '8000-awsomenesss-pp5-api-xilsbox1b5.us2.codeanyapp.com',
 ]
 
 
