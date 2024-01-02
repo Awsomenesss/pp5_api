23094741964922eb6ef1be502861fb028b3fe952
Awsomenesss <>
2024-01-02 19:37:37 +0000

    new workspace

--- bjj_api/settings.py
+++ bjj_api/settings.py
@@ -61,7 +61,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True   #'DEV' in os.environ
 
 ALLOWED_HOSTS = [os.environ.get(
     'ALLOWED_HOST'), '8000-awsomenesss-pp5-api-e56hkzej8p.us2.codeanyapp.com',
